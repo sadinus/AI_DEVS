86ae35277541e77e2289410af7b161d057b55547
Sadinus <pindelski.pawel@gmail.com>
2024-03-19 15:18:40 +0100

    Added comments

--- src/answer.ts
+++ src/answer.ts
@@ -1,3 +1,4 @@
 import { submitAnswer } from "./api";
 
+// Uzupełnij odpowiedź np. sutmitAnswer("moja odpowiedź", "mój-token")
 submitAnswer("", "");
--- src/task.ts
+++ src/task.ts
@@ -1,4 +1,5 @@
 import { authorize, getTask } from "./api";
 
+// Uzupełnij nazwę zadania w jako parametr np. authorize("helloapi")
 const { token } = await authorize("");
 getTask(token);
