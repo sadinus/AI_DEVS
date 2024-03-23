5538d9f584a616f7f2fc196a5372349bd35bc14d
Sadinus <pindelski.pawel@gmail.com>
2024-03-23 08:56:15 +0100

    Added blogger task

---
+++ src/blog.ts
@@ -0,0 +1,23 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage, SystemMessage } from "langchain/schema";
+import { submitAnswer } from "./api";
+
+const chat = new ChatOpenAI();
+
+const blogOutlines = [
+  "Wstęp: kilka słów na temat historii pizzy",
+  "Niezbędne składniki na pizzę",
+  "Robienie pizzy",
+  "Pieczenie pizzy w piekarniku",
+];
+
+chat.invoke([
+  new SystemMessage("Please write blog post for the provided outline"),
+]),
+  Promise.all(
+    blogOutlines.map((outline) =>
+      chat
+        .invoke([new HumanMessage(outline)])
+        .then((res) => res.lc_kwargs.content)
+    )
+  ).then((response) => submitAnswer(response, ""));
