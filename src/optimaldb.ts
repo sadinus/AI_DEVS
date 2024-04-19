e766805b2a5809c9158fcd9beb72d3c03d984b60
Sadinus <pindelski.pawel@gmail.com>
2024-04-19 17:22:40 +0200

    Fixed optimaldb task

--- src/optimaldb.ts
+++ src/optimaldb.ts
@@ -1,32 +1,68 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { authorize, getTask, submitAnswer } from "./api";
-import { SystemMessage } from "@langchain/core/messages";
+import { SystemMessage, HumanMessage } from "@langchain/core/messages";
 
 type Task = {
-  msg: string;
   database: string;
 };
 
 const { token } = await authorize("optimaldb");
-const { msg, database: databaseUrl } = await getTask<Task>(token);
-const chat = new ChatOpenAI();
+const { database: databaseUrl } = await getTask<Task>(token);
+const chat = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo",
+  maxTokens: 1000,
+});
 
 const database = await fetchDb(databaseUrl);
 
-const { content: shortedDatabase } = await chat.invoke([
-  new SystemMessage(`${msg} Focus on preserving all the information, but make it as short as possible. Don't replace any word, just make it more concise. \n
-  ###Database ${JSON.stringify(database)} \n
-  ###Example Input: "ania": ["Jennifer Lopez to inspiracja fitnessowa dla Ani, zw\u0142aszcza je\u015bli chodzi o taniec i ruch sceniczny.", W wolnych chwilach Ania prowadzi kana\u0142 na YouTube, gdzie dzieli si\u0119 poradami z zakresu beauty."] \n
-  ###Example Output: "ania": ["jej inspiracja fitnessowa to Jeniffer Lopez", "ma kanał na Youtube o beauty"] \n
-  Think step by step.`),
-]);
+const people = Object.keys(database);
+
+const peopleDatabase = people.map((person) => database[person]);
+
+let shortedDatabase = [];
+
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  const info = peopleDatabase[i];
+  const personInfo = await shortenPersonInfo(person, info);
+  shortedDatabase.push(personInfo);
+}
 
 console.log(shortedDatabase);
 
-submitAnswer(shortedDatabase, token);
+submitAnswer(shortedDatabase.toString(), token);
 
 async function fetchDb(url: string) {
   const response = await fetch(url).then((res) => res.json());
-
   return response;
+}
+
+async function shortenPersonInfo(person: string, info: string[]) {
+  const { content: shortedpersonInfo } = await chat.invoke([
+    new SystemMessage(`Transform string array into bullet point list that contains SAME AMOUNT OF ELEMENTS. Focus on facts. Make each bullet point more concise than array element, so it carries the same information, but in a fewer words. \n
+      ###Example Input: ANIA: ["Jennifer Lopez to inspiracja fitnessowa dla Ani, zwłaszcza jeśli chodzi o taniec i ruch sceniczny.", "W wolnych chwilach Ania prowadzi kanał na YouTube, gdzie dzieli się poradami z zakresu beauty."]
+      ###Example Output: 
+      ANIA:
+      - inspiracja fitnessowa to Jeniffer Lopez
+      - ma kanał na Youtube o beauty \n
+      Do it step by step e.g. shorten 5 elements, then next 5 and so on...
+      Like this:
+      ANIA:
+      // transform first 5 elements from array and remember where you finished
+      - ...
+      - ...
+      - ...
+      - ...
+      - ...
+      // then transform next 5 elements and continue the list
+      - ...
+      - ...
+      - ...
+      - ...
+      - ...
+      ###`),
+    new HumanMessage(`${person.toUpperCase()}: ${info.toString()}`),
+  ]);
+
+  return shortedpersonInfo.toString();
 }
